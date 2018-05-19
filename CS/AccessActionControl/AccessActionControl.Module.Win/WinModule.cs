83b76f7eaf303ec18f32df400f0dee86aaf593d9
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ CS/AccessActionControl/AccessActionControl.Module.Win/WinModule.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+using DevExpress.ExpressApp;
+
+namespace AccessActionControl.Module.Win {
+    [ToolboxItemFilter("Xaf.Platform.Win")]
+    public sealed partial class AccessActionControlWindowsFormsModule : ModuleBase {
+        public AccessActionControlWindowsFormsModule() {
+            InitializeComponent();
+        }
+    }
+}
